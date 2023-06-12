f65ff3f39085dbfbeaa88c117d66f62aa658b997
Lucas Barbosa <lucasbarbosaferreira.lb@gmail.com>
2023-06-12 17:49:35 -0400

    chore: global styles

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import StylesGlobal from './styles/styles'
 import Routes from './routes'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <StylesGlobal/>
     <Routes/>
   </React.StrictMode>
 );
--- front-end/src/pages/Home/styles.js
+++ front-end/src/pages/Home/styles.js
@@ -2,16 +2,21 @@ import styled from 'styled-components'
 
 export const Conteiner = styled.div`
     background-color: #0A0A10;
-    background-size: cover;
-
+    background-size: cover;//estica a imagem para caber ao invez do padrao de clonar 
     display: flex; 
     flex-direction: column; 
     align-items: center; 
     gap: 40px; 
+ 
+  //  height: 100vh; 
+
+
+
+
 `;
 
 export const Image = styled.img`
-position: absolute;
+
 width: 342px;
 height: 354px;
 left: 36px;
